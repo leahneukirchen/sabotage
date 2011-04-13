4cf8eedceaf384a8741c23ab5b2d57cb06acf36b
Christian Neukirchen <chneukirchen@gmail.com>
2011-04-13 12:18:25 +0200

    Update builds

--- KEEP/openssh-hacks.h
+++ KEEP/openssh-hacks.h
@@ -1,16 +1,9 @@
-#define u_short unsigned short
-#define u_long unsigned long
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+#include <sys/time.h>
+#include <sys/param.h>
 
-#define S_IWRITE S_IWUSR
-
-/* another wonderful BSD invention... :( */
-#define timercmp(a,b,CMP) (((a)->tv_sec == (b)->tv_sec) ? ((a)->tv_usec CMP (b)->tv_usec) : ((a)->tv_sec CMP (b)->tv_sec))
-#define timerclear(x) ((x)->tv_sec=(x)->tv_usec=0)
-#define timeradd(a,b,x) do { (x)->tv_sec=(a)->tv_sec+(b)->tv_sec; if (((x)->tv_usec=(a)->tv_usec+(b)->tv_usec)>=1000000) { ++(x)->tv_sec; (x)->tv_usec-=1000000; } } while (0)
-#define timersub(a,b,x) do { (x)->tv_sec=(a)->tv_sec-(b)->tv_sec; if (((x)->tv_usec=(a)->tv_usec-(b)->tv_usec)<0) { --(x)->tv_sec; (x)->tv_usec+=1000000; } } while (0)
-#define timerisset(x) ((x)->tv_sec || (x)->tv_usec)
-
-#define MAXHOSTNAMELEN       64
+#define ut_addr     ut_addr_v6[0]
 
 /* else ssh-keygen doesn't build. */
 static inline int pkcs11_init(int i) { return 0; }
